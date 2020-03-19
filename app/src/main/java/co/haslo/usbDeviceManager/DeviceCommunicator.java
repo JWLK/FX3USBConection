716dfdaafc8657014381324b18a55a376c772b5c
JWLK <jewels@haslo.co>
2020-03-19 23:19:30 +0900

    [Add] Edit DeviceCommunicator.java Error Code

--- app/src/main/java/co/haslo/usbDeviceManager/DeviceCommunicator.java
+++ app/src/main/java/co/haslo/usbDeviceManager/DeviceCommunicator.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.hardware.usb.UsbConstants;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
 
@@ -20,10 +21,11 @@ public class DeviceCommunicator {
      * Register Address Setting
      */
 
-    private static final byte CMD_SET_READN     = (byte) 0xA0;
+    private static final byte CMD_SET_READN = (byte) 0xA0;
 
     private UsbDeviceConnection mUsbDeviceConnection = null;
     private UsbInterface mUsbInterface = null;
+    private UsbEndpoint mUsbEndpoint = null;
 
 
     DeviceCommunicator(Context context, UsbDevice device) throws IOException {
@@ -31,8 +33,40 @@ public class DeviceCommunicator {
         if(manager == null){
             throw new IOException("Get USBManager Not Exist");
         }
-        mUsb
+        mUsbInterface = device.getInterface(0);
+        mUsbDeviceConnection = manager.openDevice((device));
+
+        if(mUsbDeviceConnection == null) {
+            Dlog.e("UsbDeviceConnection not Exist");
+            throw new IOException("Device Connection Error");
+        }
+
+        if(mUsbEndpoint == null) {
+            Dlog.e("UsbEndpoint not Exist");
+            throw new IOException("Device Endpoint Error");
+        }
+
+        if(!mUsbDeviceConnection.claimInterface(mUsbInterface, true)) {
+            Dlog.e("Usb ClaimInterface Test Faile");
+            throw new IOException("Device USB ClaimInterface Error");
+        }
+
+        mUsbEndpoint = mUsbInterface.getEndpoint(0);
+
     }
+
+    public void Clear() {
+        try {
+            boolean release = false;
+            release = mUsbDeviceConnection.releaseInterface(mUsbInterface);
+            Dlog.d("Device Communicator Clear" + release);
+            mUsbDeviceConnection.close();
+        } catch (Exception e) {
+            Dlog.e("Device Communicator Clear Exception Error");
+        }
+    }
+
+
 
 
 }
