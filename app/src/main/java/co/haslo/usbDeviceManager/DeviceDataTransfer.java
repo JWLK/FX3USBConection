418546608bfb6e65fc3ecf6e5e0731e577e41fa1
JWLK-HASLO <jewels@haslo.co>
2020-04-02 14:23:58 +0900

    [ADD] util/ConvertData.java

--- app/src/main/java/co/haslo/usbDeviceManager/DeviceDataTransfer.java
+++ app/src/main/java/co/haslo/usbDeviceManager/DeviceDataTransfer.java
@@ -1,5 +1,8 @@
 package co.haslo.usbDeviceManager;
 
+import co.haslo.util.ConvertData;
+import co.haslo.util.Dlog;
+
 public class DeviceDataTransfer {
 
     private static final int SEQUENCE_SIZE = 4096;
@@ -21,8 +24,40 @@ public class DeviceDataTransfer {
         }
 
         public void run(){
-            final int dataReadSize = SEQUENCE_SIZE * READ_SCANLINE_NUMBER;
-            final byte[] readBuffer = new byte[dataReadSize];
+            final int defaultReadSize = SEQUENCE_SIZE * READ_SCANLINE_NUMBER;
+            final byte[] readBuffer = new byte[defaultReadSize];
+            int readSize;
+
+            while (!isInterrupted()) {
+
+                try
+                {
+                    readSize = mDeviceCommunicator.ReadBulkTransfer(readBuffer, 0, defaultReadSize);
+
+                    if(isInterrupted()) {
+                        Dlog.i("Thread is interrupted");
+                        break;
+                    }
+                    for(int i = 0; i < defaultReadSize; i+=4) {
+                        byte Data03 = readBuffer[i + 3];
+                        byte Data02 = readBuffer[i + 2];
+                        byte Data01 = readBuffer[i + 1];
+                        byte Data00 = readBuffer[i + 0];
+                        byte[] DataArray = {Data03,Data02,Data01,Data00};
+                        Dlog.i(ConvertData.byteArrayToHexString(DataArray));
+                    }
+
+                } catch (Exception e) {
+
+                    Dlog.e("Thread Read Exception : " + e);
+
+                    readSize = -1;
+                }
+
+                if(readSize <= 0) {
+                    continue;
+                }
+            }
 
         }
 
