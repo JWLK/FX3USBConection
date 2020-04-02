1ee871dd2dcfc72061813ec793b418f42ff034f0
JWLK-HASLO <jewels@haslo.co>
2020-04-03 04:40:28 +0900

    [ADD] util/InterfaceUtil.java & usbDeviceManger/DeviceRegisterSetting.java => DataCommunication Setting Complete

---
+++ app/src/main/java/co/haslo/usbDeviceManager/DeviceRegisterSetting.java
@@ -0,0 +1,24 @@
+package co.haslo.usbDeviceManager;
+
+import co.haslo.util.Dlog;
+
+import static co.haslo.util.ConvertData.hexStringToShortArray;
+
+public class DeviceRegisterSetting {
+
+    public static void writeCommandHexData(DeviceCommunicator device, String hexString)
+    {
+        short[] dataShort = hexStringToShortArray(hexString);
+        for(short shorts : dataShort){
+            Dlog.d(String.format("0x%04X", shorts));
+        }
+
+        try {
+            device.DataTransferSingleWrite(dataShort[0], dataShort[1]);
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/co/haslo/util/InterfaceUtil.java
@@ -0,0 +1,18 @@
+package co.haslo.util;
+
+import android.widget.TextView;
+
+public class InterfaceUtil {
+
+    public static void scrollBottom(TextView textView) {
+        int lineTop =  textView.getLayout().getLineTop(textView.getLineCount()) ;
+        int scrollY = lineTop - textView.getHeight();
+        if (scrollY > 0) {
+            textView.scrollTo(0, scrollY);
+        } else {
+            textView.scrollTo(0, 0);
+        }
+    }
+
+
+}
